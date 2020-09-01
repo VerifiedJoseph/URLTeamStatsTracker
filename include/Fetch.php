c6821d7f75b4d02cc571701634fc2a60fc9df21e
Joseph <joseph@verifiedjoseph.com>
2020-09-01 16:36:03 +0100

    [Fetch] Add class

---
+++ include/Fetch.php
@@ -0,0 +1,31 @@
+<?php
+
+use Curl\Curl;
+
+class Fetch {
+
+	/** @var array $endpoint Stats endpoint */
+	private string $endpoint = 'https://tracker.archiveteam.org:1338/api/stats/';
+
+	/**
+	 * Fetch stats for a username
+	 *
+	 * @param string $username URLTeam username
+	 */
+	public function stats(string $username) {
+		$url = $this->endpoint . $username;
+
+		$curl = new Curl();
+		$curl->get($url);
+
+		if ($curl->getCurlErrorCode() !== 0) {
+			throw new Exception('Error: ' . $curl->getCurlErrorCode() . ': ' . $curl->errorMessage);
+		}
+
+		if ($curl->getHttpStatusCode() !== 200) {
+			throw new Exception('Failed to fetch: ' . $url);
+		}
+
+		return $curl->response;
+	}
+}
