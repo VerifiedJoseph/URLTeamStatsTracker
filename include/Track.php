71bf0a3aa89b93022759832cae72625653100e57
Joseph <joseph@verifiedjoseph.com>
2020-09-01 16:35:49 +0100

    [Track] Add class

---
+++ include/Track.php
@@ -0,0 +1,20 @@
+<?php
+
+class Track extends Action {
+	public function run() {
+		$data = new Data();
+		$data->setPath($this->username, $this->type);
+		$data->load();
+
+		$tdateTime = date($this->dateFormats[$this->type]);
+
+		$fetch = new Fetch();
+		$stats = $fetch->stats($this->username);
+
+		$data->update(
+			$tdateTime,
+			$stats->stats[0],
+			$stats->stats[1]
+		);
+	}
+}
