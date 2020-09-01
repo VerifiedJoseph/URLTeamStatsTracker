0ae04706ea5da1c07bbd77641a1ed123c2487dc3
Joseph <joseph@verifiedjoseph.com>
2020-09-01 16:36:19 +0100

    Create view.php

---
+++ view.php
@@ -0,0 +1,33 @@
+<?php
+/*
+	View stats for a tracked URLTeam user
+	Created: 2020-09-01
+
+	Parameters:
+	--username
+	--hourly
+	--daily
+*/
+
+include 'autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+
+include 'include/output.php';
+
+try {
+	$arguments = new Arguments();
+
+	$view = new View();
+	$view->setUsername(
+		$arguments->get('username')
+	);
+
+	$view->setUpdateType(
+		$arguments->get('update')
+	);
+
+	$view->get();
+
+} catch (Exception $e) {
+	output($e->getMessage());
+}
