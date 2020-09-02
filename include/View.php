b39459093bf025f2ce508a6f128c5fd13c01f282
Joseph <joseph@verifiedjoseph.com>
2020-09-02 23:28:28 +0100

    [View] Throw exception if data array is empty in createTable()

--- include/View.php
+++ include/View.php
@@ -19,28 +19,23 @@ class View extends Action {
 	 * Create table using CLImate
 	 *
 	 * @param array $data Data from file
+	 *
+	 * @throws Exception if data array is empty
 	 */
 	private function createTable(array $data) {
 		$table = array();
 
-		if (empty($data['data']) === false) {
-			foreach($data['data'] as $index => $item) {
-				$item['totalFound'] = number_format($item['totalFound']);
-				$item['found'] = number_format($item['found']);
-				$item['totalScanned'] = number_format($item['totalScanned']);
-				$item['scanned'] = number_format($item['scanned']);
+		if (empty($data['data'])) {
+			throw new Exception('No data to display');
+		}
 
-				$table[] = $item;
+		foreach($data['data'] as $index => $item) {
+			$item['totalFound'] = number_format($item['totalFound']);
+			$item['found'] = number_format($item['found']);
+			$item['totalScanned'] = number_format($item['totalScanned']);
+			$item['scanned'] = number_format($item['scanned']);
 
-				if ($this->type === 'hourly' && count($table) === 24) {
-					break;
-				}
-			}
-
-			$climate = new League\CLImate\CLImate;
-			$climate->table($table);
-		} else {
-			output('No data to display');
+			$table[] = $item;
 		}
 	}
 }
