bf5b380b4e5bdc7b488041cfa12d99817cdcfbe2
Joseph <joseph@verifiedjoseph.com>
2020-09-03 15:18:08 +0100

    [Helper\Validate] Add class

---
+++ include/Helper/Validate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Helper;
+
+class Validate {
+
+	/** @var string $usernameRegex Username validation regex */
+	private static string $usernameRegex = '/^([A-Za-z0-9_-]+)$/';
+
+	/**
+	 * Validate a username
+	 *
+	 * @param string $username URLTeam username
+	 * @return boolean
+	 */
+	public static function username(string $username) {
+		if (preg_match(self::$usernameRegex, $username)) {
+			return true;
+		}
+
+		return false;
+	}
+}
