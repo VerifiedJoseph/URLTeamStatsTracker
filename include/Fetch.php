51a7646d9168756dfd8dcac1179e47a6264bd234
Joseph <joseph@verifiedjoseph.com>
2020-10-02 13:47:12 +0100

    [Fetch] Add request retry with a 5 second delay

--- include/Fetch.php
+++ include/Fetch.php
@@ -7,6 +7,12 @@ class Fetch {
 	/** @var array $endpoint Stats endpoint */
 	private string $endpoint = 'https://tracker.archiveteam.org:1338/api/stats/';
 
+	/** @var int $maxRetries Number of max retries */
+	private int $maxRetries = 3;
+
+	/** @var int $retryDelay Number of second to delay each retry */
+	private int $retryDelay = 5;
+
 	/**
 	 * Fetch stats for a username
 	 *
@@ -19,6 +25,15 @@ class Fetch {
 		$url = $this->endpoint . $username;
 
 		$curl = new Curl();
+
+		$curl->setRetry(function ($instance) {
+			if ($instance->retries < $this->maxRetries) {
+				sleep($this->retryDelay);
+				return true;
+			}
+			return false;
+		});
+		
 		$curl->get($url);
 
 		if ($curl->getCurlErrorCode() !== 0) {
